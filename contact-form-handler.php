59de2232d3d338cbdd3c6c0293ab2715b7e76889
Jesyiii16 <johncarlosuntalan16@gmail.com>
2025-11-12 10:47:25 +0800

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,9 +1,9 @@
 <?php
 
-    $name = $_GET['name'];
-    $visitor_email = $_GET['email'];
-    $subject = $_GET['subject'];
-    $message = $_GET['message'];
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
     
     
 
@@ -28,4 +28,5 @@
     
 
 ?>
+
 
